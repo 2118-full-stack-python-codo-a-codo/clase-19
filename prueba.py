cebb6af60a40bc27339b558177d38ff7fe5caecc
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-06-07 20:05:37 -0300

    first python script

---
+++ prueba.py
@@ -0,0 +1,11 @@
+
+def main():
+    print("Hello World")
+    print("otra instruccion")
+    holaSoyUnaVariable = 4
+    holaSoyUnaVariable = "Hola de nuevo"
+    print(holaSoyUnaVariable)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
